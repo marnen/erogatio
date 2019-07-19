c5b4dbbadf4efcc9eae71c13abd1559e7e8e1219
Marnen Laibow-Koser <marnen@marnen.org>
2019-07-18 23:43:17 -0400

    Use a more reasonable factory for right now. [#4]

--- spec/factories/work_unit.rb
+++ spec/factories/work_unit.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :work_unit do
     date { Faker::Date.backward 365 }
     description { Faker::Lorem.sentence }
-    hours { rand(1..600).minutes }
+    hours { rand(1..10).hours } # TODO: support minutes too
     association :user
   end
 end
