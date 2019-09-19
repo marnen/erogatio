cc263515f26de67d9f1923ea7c4671705e1b28f2
Marnen Laibow-Koser <marnen@marnen.org>
2019-09-19 14:24:57 -0400

    Display description for work unit. [#21]

--- features/step_definitions/work_unit_steps.rb
+++ features/step_definitions/work_unit_steps.rb
@@ -6,16 +6,18 @@ Given 'another user has a work unit' do
   create :work_unit
 end
 
-When /^I enter (\d+) hours? of work on (.*)$/ do |hours, date|
+When /^I enter (\d+) hours? of work for "([^"]+)" on (.*)$/ do |hours, description, date|
   visit new_work_unit_path
   select_date date, from: 'Date'
   fill_in 'Hours', with: hours
+  fill_in 'Description', with: description
   click_button 'Save'
 end
 
-Then /^I should see (\d+) hours? of work on (.*)$/ do |hours, date|
+Then /^I should see (\d+) hours? of work for "([^"]+)" on (.*)$/ do |hours, description, date|
   found_unit = page.all '.work-unit' do |work_unit|
-    [/#{hours} hours?/, date].all? {|content| work_unit.has_content? content }
+    {hours: /#{hours} hours?/, description: description, date: date}.all? {|css, content| work_unit.has_css? ".#{css}", text: content }
+    # [/#{hours} hours?/, description, date].all? {|content| work_unit.has_content? content }
   end
   expect(found_unit.size).to be == 1
 end
