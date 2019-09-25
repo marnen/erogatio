7c03cdcb4372fd949cc8cd90a591f7e95497b5dd
Marnen Laibow-Koser <marnen@marnen.org>
2019-09-25 17:59:16 -0400

    Extract before_action. [#5]

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,11 @@
 class PaymentsController < ApplicationController
+  before_action :load_work_unit!
+
   def new
-    load_work_unit!
     @payment = Payment.new date: Date.today, amount: @work_unit.pay
   end
 
   def create
-    load_work_unit!
     @work_unit.payments.create! params.require(:payment).permit :date, :amount
     redirect_to work_units_path
   end
