c9b5b0996bdea45c834bcb5ac800ca066a164a89
Marnen Laibow-Koser <marnen@marnen.org>
2019-09-25 18:24:41 -0400

    Extract shared examples. [#5]

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -11,9 +11,5 @@ RSpec.describe Payment, type: :model do
     it { is_expected.to validate_presence_of :work_unit_id }
   end
 
-  describe '.permitted_params' do
-    it 'includes all the content field names except the timestamps' do
-      expect(Payment.permitted_params).to contain_exactly *Payment.content_columns.map(&:name).reject {|name| name.ends_with? '_at' }
-    end
-  end
+  include_examples 'permit content columns'
 end
--- spec/models/work_unit_spec.rb
+++ spec/models/work_unit_spec.rb
@@ -12,11 +12,7 @@ RSpec.describe WorkUnit, type: :model do
     it { is_expected.to validate_presence_of :user_id }
   end
 
-  describe '.permitted_params' do
-    it 'includes all the content field names except the timestamps' do
-      expect(WorkUnit.permitted_params).to contain_exactly *WorkUnit.content_columns.map(&:name).reject {|name| name.ends_with? '_at' }
-    end
-  end
+  include_examples 'permit content columns'
 
   describe '#paid?' do
     let(:work_unit) { FactoryBot.create :work_unit }
